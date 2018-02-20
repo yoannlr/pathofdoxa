36737c74e160acd1e0355170cee64db006925e58
yoannlr <yoann22lr@gmail.com>
2018-02-20 21:27:51 +0100

    states !

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <map>
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 
 using namespace std;
 
 const int WIDTH = 1280;
 const int HEIGHT = 720;
-
-std::map<int, bool> keys;
+const int FPS = 60;
+const int REFRESHPERIOD = 1000 / FPS;
+const int TICKRATE = 20;
+const int TICKPERIOD = 1000 / TICKRATE;
+const char TITLE[] = "Path of doxa";
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
-SDL_Event event;
+
 bool running = true;
+
+std::map<int, bool> keys;
+
+SDL_Event event;
 
 class Texture {
 public:
@@ -44,32 +51,43 @@ public:
 	}
 };
 
+class State {
+public:
+	virtual void init() = 0;
+	virtual void tick(int dt) = 0;
+	virtual void render(SDL_Renderer *renderer) = 0;
+	virtual void keyPressed(int key) = 0;
+	virtual void keyReleased(int key) = 0;
+};
+
+class TestState : public State {
+public:
+	void init() {}
+	void tick(int dt) {}
+	void render(SDL_Renderer *renderer) {}
+	void keyPressed(int key) {}
+	void keyReleased(int key) {}
+};
+
 static bool isKeyDown(char key) {
 	return keys[int(key)];
 }
 
-void handleKeyEvent(SDL_KeyboardEvent *key) {
-	if(key->type == SDL_KEYUP) {
-		keys[key->keysym.sym] = false;
-	} else {
-		keys[key->keysym.sym] = true;
-	}
-}
-
-int x = 0;
 
 int main(int argc, char const *argv[])
 {
+	int lastTickTime = 0, lastDispTime = 0, currentTime;
+
+	TestState state;
+	State *currentState = &state;
+
 	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		cout << SDL_GetError() << endl;
 		return 1;
 	}
 
-	window = SDL_CreateWindow("Path of doxa", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-	
-	Texture img("img.bmp", renderer);
 
 	while(running) {
 		while(SDL_PollEvent(&event)) {
@@ -79,20 +97,35 @@ int main(int argc, char const *argv[])
 				break;
 			case SDL_KEYDOWN:
 			case SDL_KEYUP:
-				handleKeyEvent(&event.key);
+				SDL_KeyboardEvent *key = &event.key;
+				if(key->type == SDL_KEYUP) {
+					keys[key->keysym.sym] = false;
+					currentState->keyReleased(key->keysym.sym);
+				} else {
+					keys[key->keysym.sym] = true;
+					currentState->keyPressed(key->keysym.sym);
+				}
 				break;
+			}
 		}
-	}
 
-		if(isKeyDown('q')) x--;
-		if(isKeyDown('d')) x++;
+		currentTime = SDL_GetTicks();
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
+		if(currentTime > lastTickTime + TICKPERIOD) {
+			lastTickTime = currentTime;
+			currentState->tick(currentTime - lastTickTime);
+		}
 
-		img.render(renderer, x, 10, 100, 100, 0, 0, 20, 20);
+		if(currentTime > lastDispTime + REFRESHPERIOD) {
+			lastDispTime = currentTime;
 
-		SDL_RenderPresent(renderer);
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+			SDL_RenderClear(renderer);
+
+			currentState->render(renderer);
+
+			SDL_RenderPresent(renderer);
+		}
 	}
 
 	SDL_Quit();
